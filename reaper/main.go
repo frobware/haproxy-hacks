580ca6b2e8d8426839cf701c143a9936fa1850d5
Andrew McDermott <aim@frobware.com>
2020-01-13 08:34:18 +0000

    Add reaper
    Showcases `cmd.CombinedOutput()` failing.
    
    The `StartReaper()` go routine occasionally reaps the PID that cmd is
    waiting on:
    
    ```console
    StartReaper: reaped child 1096
     - Checking http://localhost:80 ...
     - Health check ok : 0 retry attempt(s).
    
    StartReaper: reaped child 1110
     - Checking http://localhost:80 ...
     - Health check ok : 0 retry attempt(s).
    
    StartReaper: reaped child 1124
     - Checking http://localhost:80 ...
     - Health check ok : 0 retry attempt(s).
    
    StartReaper: reaped child 1138
     - Checking http://localhost:80 ...
     - Health check ok : 0 retry attempt(s).
    
    StartReaper: reaped child 1152
    StartReaper: reaped child 1205
    cmd.CombinedOutput() pid == 1205
    ran for 10.866933924 seconds
    panic: wait: no child processes
    
    goroutine 1 [running]:
    main.main()
    	/home/aim/haproxy-hacks/reaper/main.go:26 +0x288
    make: *** [Makefile:4: run] Error 2
    ```

---
+++ reaper/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+
+	defer func() {
+		fmt.Fprintf(os.Stderr, "ran for %v seconds\n", time.Now().Sub(startTime).Seconds())
+	}()
+
+	go StartReaper()
+
+	for {
+		cmd := exec.Command("/bin/reload-haproxy")
+
+		if out, err := cmd.CombinedOutput(); err != nil {
+			fmt.Fprintf(os.Stderr, "cmd.CombinedOutput() pid == %v\n", cmd.Process.Pid)
+			panic(err.Error())
+		} else {
+			fmt.Println(string(out))
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func StartReaper() {
+	if os.Getpid() != 1 {
+		return
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGCHLD)
+
+	for {
+		<-sigs
+
+		for {
+			cpid, err := syscall.Wait4(-1, nil, syscall.WNOHANG, nil)
+
+			for err == syscall.EINTR {
+				cpid, err = syscall.Wait4(-1, nil, syscall.WNOHANG, nil)
+			}
+
+			if cpid < 1 {
+				break
+			}
+
+			fmt.Printf("StartReaper: reaped child %v\n", cpid)
+		}
+	}
+}
