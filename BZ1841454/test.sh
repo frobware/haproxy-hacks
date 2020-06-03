fd73cf3818ac8b321ed5b74043a41bea94a3136e
Andrew McDermott <aim@frobware.com>
2020-06-03 13:44:15 +0100

    BZ1841454: initial commit

---
+++ BZ1841454/test.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+go_mod="$(sed -e 's/^/        /' go.mod    | sed '/^[[:space:]]*$/d')"
+go_sum="$(sed -e 's/^/        /' go.sum    | sed '/^[[:space:]]*$/d')"
+
+go_server="$(sed -e 's/^/        /' server/server.go | sed '/^[[:space:]]*$/d')"
+
+cat <<EOF
+apiVersion: v1
+kind: List
+items:
+- apiVersion: v1
+  kind: Service
+  metadata:
+    name: bz1841454
+  spec:
+    selector:
+      app: bz1841454
+    ports:
+      - port: 8080
+        name: http
+        targetPort: 8080
+        protocol: TCP
+- apiVersion: v1
+  kind: ConfigMap
+  labels:
+    app: bz1841454
+  metadata:
+    name: src-config
+  data:
+    go.mod: |
+$go_mod
+    go.sum: |
+$go_sum
+    server.go: |
+$go_server
+- apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: bz1841454
+    labels:
+      app: bz1841454
+  spec:
+    replicas: ${REPLICAS:-2}
+    template:
+      metadata:
+        name: bz1841454
+        labels:
+          app: bz1841454
+      spec:
+        initContainers:
+        - image: golang:1.14
+          name: builder
+          command: ["/bin/bash", "-c"]
+          args:
+          - set -e;
+            cd /go/src;
+            go build -o /workdir/server -x -v -mod=readonly server.go;
+          env:
+          - name: GO111MODULE
+            value: "auto"
+          - name: GOCACHE
+            value: "/workdir"
+          volumeMounts:
+          - name: src-volume
+            mountPath: /go/src
+          - name: tmp
+            mountPath: /var/run
+          - name: workdir
+            mountPath: /workdir
+        volumes:
+        - name: src-volume
+          configMap:
+            name: src-config
+        - name: tmp
+          emptyDir: {}
+        - name: workdir
+          emptyDir: {}
+        containers:
+        - image: golang:1.14
+          name: server
+          command: ["/workdir/server"]
+          env:
+          - name: BUSY_TIMEOUT
+            value: "${BUSY_TIMEOUT:-0}"
+          volumeMounts:
+          - name: workdir
+            mountPath: /workdir
+          - name: tmp
+            mountPath: /var/run
+        volumes:
+        - name: src-volume
+          configMap:
+            name: src-config
+        - name: workdir
+          emptyDir: {}
+        - name: tmp
+          emptyDir: {}
+    selector:
+      matchLabels:
+        app: bz1841454
+- apiVersion: route.openshift.io/v1
+  kind: Route
+  metadata:
+    labels:
+      app: bz1841454
+    name: bz1841454-edge
+  spec:
+    port:
+      targetPort: 8080
+    tls:
+      termination: edge
+      insecureEdgeTerminationPolicy: Redirect
+    to:
+      kind: Service
+      name: bz1841454
+      weight: 100
+    wildcardPolicy: None
+EOF
