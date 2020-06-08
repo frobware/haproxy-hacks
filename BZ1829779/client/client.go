883d9203ad0561bc3688b0d577877d39dc29907e
Andrew McDermott <aim@frobware.com>
2020-06-08 10:46:23 +0100

    BZ1829779: initial setup

---
+++ BZ1829779/client/client.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+// Fetcher fetches HTML documents.
+type Fetcher interface {
+	// Fetch returns a reader for the body of the downloaded URL,
+	// or error if it could not be downloaded. The caller is
+	// responsible for body.Close().
+	Fetch(url string) (body io.ReadCloser, err error)
+}
+
+// Ensure fetcher is a Fetcher.
+var _ Fetcher = (*fetcher)(nil)
+
+type fetcher struct {
+	FetchTimeout time.Duration
+	Fetcher
+}
+
+type request struct {
+	Fetcher
+	URL string
+}
+
+// result captures all of the state after downloading request.URL.
+type result struct {
+	request
+	getDuration time.Duration
+	error       error
+}
+
+func fetch(req request) *result {
+	startTime := time.Now()
+	result := &result{request: req}
+	body, err := req.Fetcher.Fetch(req.URL)
+
+	if err != nil {
+		result.error = err
+		return result
+	}
+
+	defer body.Close()
+	result.getDuration = time.Now().Sub(startTime)
+	return result
+}
+
+func startWorkers(maxWorkers int, done <-chan struct{}, requests <-chan request, results chan<- *result) *sync.WaitGroup {
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < maxWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case req := <-requests:
+					results <- fetch(req)
+				case <-done:
+					return
+				}
+			}
+		}()
+	}
+
+	return wg
+}
+
+// Fetch URL returning the reader to the body of the document, or an
+// error if URL could not be fetched. The caller must call Close() on
+// the reader to avoid resource leaks.
+func (f fetcher) Fetch(URL string) (io.ReadCloser, error) {
+	client := http.Client{}
+
+	if f.FetchTimeout > 0 {
+		client.Timeout = f.FetchTimeout
+	}
+
+	resp, err := client.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		msg := fmt.Sprintf("fetch failed: HTTP status %d", resp.StatusCode)
+		return nil, errors.New(msg)
+	}
+
+	return resp.Body, nil
+}
+
+// NewHTTPFetcher returns a new Fetcher.
+func NewHTTPFetcher(fetchTimeout time.Duration) *fetcher {
+	return &fetcher{
+		FetchTimeout: fetchTimeout,
+	}
+}
+
+var (
+	verbose        = flag.Bool("v", false, "Verbose")
+	workers        = flag.Int("workers", 50, "number of GET workers")
+	timeout        = flag.Duration("timeout", 100*time.Millisecond, "GET timeout")
+	concurrentGets = flag.Int("c", 100, "concurrent GET requests")
+)
+
+func main() {
+	flag.Parse()
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
+	doneCh := make(chan struct{})
+	requestCh := make(chan request)
+	resultCh := make(chan *result)
+
+	fetcher := NewHTTPFetcher(*timeout)
+	wg := startWorkers(*workers, doneCh, requestCh, resultCh)
+
+	var pending []request
+
+	for i := 0; i < *concurrentGets; i++ {
+		pending = append(pending, request{
+			Fetcher: fetcher,
+			URL:     flag.Arg(0),
+		})
+
+	}
+
+	successCh := make(chan time.Duration)
+
+	go func() {
+		var n int
+		values := make([]int64, *workers, *workers)
+		ticker := time.Tick(3 * time.Second)
+
+		for {
+			select {
+			case duration := <-successCh:
+				values[n%len(values)] = duration.Milliseconds()
+				n = n + 1
+			case <-ticker:
+				var total int64 = 0
+				for i := range values {
+					total += values[i]
+				}
+				var avg int64 = total / int64(len(values))
+				log.Println("Avg GET", time.Duration(avg)*time.Millisecond)
+			}
+		}
+	}()
+
+	outstandingFetches := 0
+
+	for {
+		var sendCh chan<- request
+		var link request
+
+		if len(pending) > 0 {
+			sendCh = requestCh
+			link = pending[0]
+		} else if outstandingFetches == 0 {
+			break
+		}
+
+		select {
+		case sendCh <- link:
+			outstandingFetches++
+			pending = pending[1:]
+		case result := <-resultCh:
+			outstandingFetches--
+			if result.error != nil {
+				log.Println(fetcher.FetchTimeout, result.error.Error())
+			} else {
+				successCh <- result.getDuration
+			}
+			pending = append(pending, request{
+				Fetcher: result.Fetcher,
+				URL:     result.URL,
+			})
+		}
+	}
+
+	close(doneCh)
+	wg.Wait()
+}
---
+++ BZ1829779/make-additional-backends.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+for i in $(seq 1 1000); do
+    cat <<EOF
+backend be_http:default:helloworld-${i}
+  mode http
+  option redispatch
+  option forwardfor
+  balance leastconn
+
+  timeout check 5000ms
+  http-request set-header X-Forwarded-Host %[req.hdr(host)]
+  http-request set-header X-Forwarded-Port %[dst_port]
+  http-request set-header X-Forwarded-Proto http if !{ ssl_fc }
+  http-request set-header X-Forwarded-Proto https if { ssl_fc }
+  http-request set-header X-Forwarded-Proto-Version h2 if { ssl_fc_alpn -i h2 }
+  # Forwarded header: quote IPv6 addresses and values that may be empty as per https://tools.ietf.org/html/rfc7239
+  http-request add-header Forwarded for=%[src];host=%[req.hdr(host)];proto=%[req.hdr(X-Forwarded-Proto)];proto-version=\"%[req.hdr(X-Forwarded-Proto-Version)]\"
+  cookie a5313a2bcc9e845108addb4bdf97a72d insert indirect nocache httponly
+EOF
+done
