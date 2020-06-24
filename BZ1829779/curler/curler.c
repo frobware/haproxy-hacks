485dfb078f377630493c8d493662cfce864aebeb
Andrew McDermott <aim@frobware.com>
2020-06-24 10:46:29 +0100

    Add curler

---
+++ BZ1829779/curler/curler.c
@@ -0,0 +1,91 @@
+#include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#undef NDEBUG
+#include <assert.h>
+
+static size_t write_cb(void *data, size_t size, size_t nmemb, void *userp) {
+  size_t realsize = size * nmemb;
+  return realsize;
+}
+
+int main(int argc, char *argv[]) {
+  CURL *curl_handle;
+  CURLcode res;
+  int i, n = 10;
+
+  if (argc < 2) {
+    fprintf(stderr, "usage: <host>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  if (getenv("N") != NULL) {
+    n = atoi(getenv("N"));
+  }
+
+  for (i = 0; i < n; i++) {
+    curl_handle = curl_easy_init();
+    assert(curl_handle);
+
+    if (curl_handle) {
+      long longinfo;
+      double doubleinfo;
+
+      curl_easy_setopt(curl_handle, CURLOPT_URL, argv[1]);
+      curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb);
+
+      curl_easy_setopt(curl_handle, CURLOPT_SSL_VERIFYPEER, 0L);
+      curl_easy_setopt(curl_handle, CURLOPT_SSL_VERIFYHOST, 0L);
+
+      /* client.Get() */
+      res = curl_easy_perform(curl_handle);
+
+      if (res != CURLE_OK) {
+	fprintf(stderr, "curl_easy_perform() failed: %s\n",
+		curl_easy_strerror(res));
+      } else {
+	assert((CURLE_OK == curl_easy_getinfo(curl_handle,
+					      CURLINFO_NAMELOOKUP_TIME,
+					      &doubleinfo)));
+	fprintf(stdout, "namelookup %0.6f ", doubleinfo);
+
+	assert(
+	    (CURLE_OK == curl_easy_getinfo(curl_handle, CURLINFO_CONNECT_TIME,
+					   &doubleinfo)));
+	fprintf(stdout, "connect %0.6f ", doubleinfo);
+
+	assert((CURLE_OK == curl_easy_getinfo(curl_handle,
+					      CURLINFO_PRETRANSFER_TIME,
+					      &doubleinfo)));
+	fprintf(stdout, "pre %0.6f ", doubleinfo);
+
+	assert((CURLE_OK == curl_easy_getinfo(curl_handle,
+					      CURLINFO_STARTTRANSFER_TIME,
+					      &doubleinfo)));
+	fprintf(stdout, "start %0.6f ", doubleinfo);
+
+	assert(
+	    (CURLE_OK == curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE,
+					   &longinfo)));
+	fprintf(stdout, "http_code %03ld ", longinfo);
+
+	assert((CURLE_OK == curl_easy_getinfo(curl_handle, CURLINFO_LOCAL_PORT,
+					      &longinfo)));
+	fprintf(stdout, "port %ld ", longinfo);
+
+	assert((CURLE_OK == curl_easy_getinfo(curl_handle, CURLINFO_TOTAL_TIME,
+					      &doubleinfo)));
+	fprintf(stdout, "total %0.6f\n", doubleinfo);
+      }
+
+      curl_easy_cleanup(curl_handle); // End a libcurl easy handle
+    }
+  }
+
+  curl_global_cleanup();
+
+  return 0;
+}
