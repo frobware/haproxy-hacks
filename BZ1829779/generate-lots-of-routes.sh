14762e98854e21b51f22561af67602b4c2b86182
Andrew McDermott <aim@frobware.com>
2020-07-22 10:33:59 +0100

    BZ1829779: update scripts that generate services
    Former-commit-id: cf1d0cd5f331a5d5de17376b74a47ad182cadfc7

--- BZ1829779/generate-lots-of-routes.sh
+++ BZ1829779/generate-lots-of-routes.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-oc new-project lots-of-routes
-./generate-pods.sh 1 | oc apply -f -
-./generate-services.sh ${1:-100} | oc apply -f -
+./generate-pods.sh ${1:-100} | oc ${OP:-apply} -f -
+./generate-services.sh ${1:-100} | oc ${OP:-apply} -f -
 ./expose-services.sh ${1:-100}
