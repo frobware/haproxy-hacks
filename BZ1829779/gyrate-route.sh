9d20dfedf7857b435d7e22595e55ee87d0eb41f8
Andrew McDermott <aim@frobware.com>
2020-09-18 15:35:33 +0100

    BZ1829779/gyrate-route: change to specific service

--- BZ1829779/gyrate-route.sh
+++ BZ1829779/gyrate-route.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-for i in $(seq 1 ${1:-10}); do
-    oc delete -n lots-of-routes route helloworld-${i}
-done
+# for i in $(seq 1 ${1:-10}); do
+#     oc delete -n lots-of-routes route helloworld-${i}
+# done
 
-for i in $(seq 1 ${1:-10}); do
-    oc expose -n lots-of-routes service helloworld-${i}
+# for i in $(seq 1 ${1:-10}); do
+#     oc expose -n lots-of-routes service helloworld-${i}
+# done
+
+while :
+do
+    oc delete service/my-service
+    sleep 1.5
+    oc expose pod helloworld-1 --type=ClusterIP --name=my-service --port 8080
+    sleep 2.5
 done
