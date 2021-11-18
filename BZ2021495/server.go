477beca38046029df7076cfaaab9ee94f2dd05a1
Andrew McDermott <aim@frobware.com>
2021-11-18 15:03:41 +0000

    BZ2007581 snapshot
    Former-commit-id: 852ffbd6ff813cf252ea1fc1e3e7629265294732

--- BZ2021495/server.go
+++ BZ2021495/server.go
@@ -6,17 +6,23 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("connection from %s\n", r.RemoteAddr)
-		fmt.Fprintln(w, r.Proto, r.RemoteAddr)
-	})
+type handler struct {
+	Port int
 
-	for i := 9000; i < 9003; i++ {
+	http.Handler
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%d connection from %s\n", h.Port, r.RemoteAddr)
+	fmt.Fprintln(w, r.Proto, r.RemoteAddr)
+}
+
+func main() {
+	for i := 9000; i < 9006; i++ {
 		go func(port int) {
 			addr := fmt.Sprintf(":%v", port)
 			log.Printf("listening at %s\n", addr)
-			err := http.ListenAndServe(addr, nil)
+			err := http.ListenAndServe(addr, handler{Port: port})
 			if err != nil {
 				log.Fatalf("ListenAndServe: %v\n", err)
 			}
