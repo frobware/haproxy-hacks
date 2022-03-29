b83daa02bb4aac13d9b8bb3a2ab5e01271b5a3c5
Andrew McDermott <aim@frobware.com>
2022-03-29 10:26:01 +0100

    BZ2068484; Add socat.sh

---
+++ BZ2068484/socat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+socat -T 1 -d -d tcp-l:${1:-1936},reuseaddr,fork,crlf system:"echo -e \"\\\"HTTP/1.0 200 OK\\\nDocumentType: text/html\\\nHeader: Set-Cookie "X=Y";\\\n\\\n<html>date: \$\(date\)<br>server:\$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT<br>client: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\\\n<pre>\\\"\"; cat; echo -e \"\\\"\\\n</pre></html>\\\"\""
