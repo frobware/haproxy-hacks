7d9293abd886c72b035825983f6311f93b25c4d7
Andrew McDermott <aim@frobware.com>
2022-04-25 11:32:12 +0100

    maxconn: remove redundant code

--- maxconn/maxconn-analysis.sh
+++ maxconn/maxconn-analysis.sh
@@ -39,48 +39,16 @@ function thread_values() {
     echo $(sqlite3 $db "select distinct(threads) from results")
 }
 
-function a() {
-    for balance_algorithm in $(algorithm_values); do
-        echo "# $balance_algorithm"
-        for weight in $(weight_values); do
-            # echo "## $balance_algorithm: weight = $weight"
-            for backend_count in $(backend_values); do
-                # echo "# $balance_algorithm: weight = $weight, backends = $backend_count"
-                for thread_count in $(thread_values); do
-                    echo "## weight=$weight backends=$backend_count threads=$thread_count"
-                    echo "\`\`\`\`console"
-                    echo "\`\`\`sh"
-                    sqlite3 $db <<EOF
-.headers on
-.mode column
-.width -1 -1 -1 -1
-SELECT maxconn, actual_maxconn as "maxconn (HAProxy)", actual_maxsock as "maxsock (HAProxy)", process_size as "Process Size (KB)"
-  FROM results
- WHERE weight == $weight and
-       balance_algorithm == "$balance_algorithm" and
-       backends == $backend_count and
-       threads == $thread_count
-ORDER BY actual_maxconn, process_size
-EOF
-                done
-                echo "\`\`\`"
-                echo "\`\`\`\`"
-            done
-        done
-    done
-}
-
-function b() {
-    for balance_algorithm in $(algorithm_values); do
-        echo "# $balance_algorithm"
-        for weight in $(weight_values); do
-            # echo "## $balance_algorithm: weight = $weight"
-            for backend_count in $(backend_values); do
-                # echo "# $balance_algorithm: weight = $weight, backends = $backend_count"
-                for thread_count in $(thread_values); do
-                    echo "## algorithm=$balance_algorithm weight=$weight backends=$backend_count threads=$thread_count"
-                    printf "\`\`\`\n";
-                    sqlite3 $db <<EOF
+for balance_algorithm in $(algorithm_values); do
+    echo "# $balance_algorithm"
+    for weight in $(weight_values); do
+        # echo "## $balance_algorithm: weight = $weight"
+        for backend_count in $(backend_values); do
+            # echo "# $balance_algorithm: weight = $weight, backends = $backend_count"
+            for thread_count in $(thread_values); do
+                echo "## algorithm=$balance_algorithm weight=$weight backends=$backend_count threads=$thread_count"
+                printf "\`\`\`\n";
+                sqlite3 $db <<EOF
 .headers on
 .mode column
 .width -1 -1 -1 -1
@@ -92,11 +60,8 @@ SELECT maxconn, actual_maxconn as "maxconn (HAProxy)", actual_maxsock as "maxsoc
        threads == $thread_count
 ORDER BY actual_maxconn, process_size
 EOF
-                    printf "\`\`\`\n";
-                done
+                printf "\`\`\`\n";
             done
         done
     done
-}
-
-b
+done
