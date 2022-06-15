2dbcd0abd2c12d8d514c99044f4282c89a541eb8
Andrew McDermott <aim@frobware.com>
2022-06-15 17:47:35 +0100

    keda: snapshot

--- keda/setup/setup.sh
+++ keda/setup/setup.sh
@@ -2,14 +2,15 @@
 
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-# oc create serviceaccount thanos
-# oc describe serviceaccount thanos
+oc apply -f $thisdir/configmap.yaml
+oc delete -n openshift-ingress-operator serviceaccount thanos
+sleep 5
+oc create -n openshift-ingress-operator serviceaccount thanos
+oc describe -n openshift-ingress-operator serviceaccount thanos
+oc apply -n openshift-ingress-operator -f $thisdir/role.yaml
+#oc apply -n openshift-ingress-operator -f $thisdir/rolebinding.yaml
+secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{print $1 }')
+oc process TOKEN=$secret -f $thisdir/triggerauthentication.yaml | oc apply -n openshift-ingress-operator -f -
+oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
+oc get triggerauthentications.keda.sh -o yaml | grep thanos-token
 
-oc apply -f role.yaml
-oc apply -f rolebinding.yaml
-
-SECRET=$(oc get secret -n openshift-ingress-operator | grep ingress-operator-token | head -n 1 | awk '{print $1 }')
-oc process TOKEN=$SECRET -f triggerauthentication.yaml | oc apply -f -
-
-# oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
-oc adm policy add-role-to-user thanos-metrics-reader -z ingress-operator --role-namespace=openshift-ingress-operator
