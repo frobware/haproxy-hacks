c2760e693dcb6e84e8b532b48f34662c22a72ba9
Andrew McDermott <aim@frobware.com>
2022-06-15 19:04:16 +0100

    keda: snapshot

--- keda/setup/setup.sh
+++ keda/setup/setup.sh
@@ -12,5 +12,6 @@ oc apply -n openshift-ingress-operator -f $thisdir/role.yaml
 secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{print $1 }')
 oc process TOKEN=$secret -f $thisdir/triggerauthentication.yaml | oc apply -n openshift-ingress-operator -f -
 oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
+oc adm policy add-role-to-user thanos-metrics-reader -z ingress-operator --role-namespace=openshift-ingress-operator
 oc get triggerauthentications.keda.sh -o yaml | grep thanos-token
 
