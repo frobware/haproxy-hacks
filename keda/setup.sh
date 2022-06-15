e0c757b50ca257a0bcaa3bffe94bb2315060280c
Andrew McDermott <aim@frobware.com>
2022-06-15 13:53:28 +0100

    keda

--- keda/setup.sh
+++ keda/setup.sh
@@ -5,4 +5,5 @@ oc logs deployment.apps/test-app
 oc create serviceaccount thanos
 oc describe serviceaccount thanos
 oc create -f triggerauthentication.yaml
+oc create -f role.yaml
 oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
