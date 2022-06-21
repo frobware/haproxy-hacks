385acdf18ba0ac95a119d9897cdfd124ccdaed31
Andrew McDermott <aim@frobware.com>
2022-06-21 15:52:44 +0100

    keda: snap

--- keda/setup/create-trigger-authentication.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-SECRET=$(oc get secret -n openshift-ingress-operator | grep ingress-operator-token | head -n 1 | awk '{print $1 }')
-oc process TOKEN=$SECRET -f $thisdir/triggerauthentication.yaml | oc replace --force -f -
-
-
-
