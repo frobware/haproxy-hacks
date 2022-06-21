c9784a1fe7a0a5dbf21fa2588744a42c43011377
Andrew McDermott <aim@frobware.com>
2022-06-21 15:30:10 +0100

    keda: snap

--- keda/setup/cleanup.sh
+++ keda/setup/cleanup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+pushd $thisdir > /dev/null 2>&1
+
 oc adm policy remove-role-from-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
-oc delete -n openshift-ingress-operator -f $thisdir/role.yaml
+oc delete -n openshift-ingress-operator -f role.yaml
 oc delete serviceaccount thanos
