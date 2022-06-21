644a50b9261112783eb1669b5ad05046c41b786b
Andrew McDermott <aim@frobware.com>
2022-06-21 15:23:58 +0100

    keda: snap

--- keda/setup/setup.sh
+++ keda/setup/setup.sh
@@ -3,14 +3,14 @@
 set -eu
 
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+pushd $thisdir > /dev/null 2>&1
 
-oc apply -f $thisdir/configmap.yaml
-oc get -n openshift-ingress-operator sa/thanos && \
-    oc delete -n openshift-ingress-operator serviceaccount thanos
+oc apply -f configmap.yaml
+oc get -n openshift-ingress-operator serviceaccount/thanos && oc delete -n openshift-ingress-operator serviceaccount thanos
 oc create -n openshift-ingress-operator serviceaccount thanos
 oc describe -n openshift-ingress-operator serviceaccount thanos
-oc apply -n openshift-ingress-operator -f $thisdir/role.yaml
-secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{print $1 }')
-oc process TOKEN=$secret -f $thisdir/triggerauthentication.yaml | oc apply -n openshift-ingress-operator -f -
+oc apply -n openshift-ingress-operator -f role.yaml
+secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{print $1 }'w)
+oc process TOKEN=$secret -f triggerauthentication.yaml | oc apply -n openshift-ingress-operator -f -
 oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
 oc get triggerauthentications.keda.sh -o yaml | grep $secret
