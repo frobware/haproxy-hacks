0676d3534ee2b371b4b058820acd7739efb4d5bc
Andrew McDermott <aim@frobware.com>
2022-06-22 10:25:42 +0100

    keda: snap

--- keda/nodes-ready-app/go.mod
+++ keda/nodes-ready-app/go.mod
@@ -1,12 +1,13 @@
 module nodes-ready-app
 
-go 1.17
+go 1.18
 
 require (
 	github.com/prometheus/client_golang v1.12.2
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	k8s.io/api v0.24.2
 	k8s.io/client-go v0.24.2
+	k8s.io/klog/v2 v2.60.1
 )
 
 require (
@@ -49,7 +50,6 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/apimachinery v0.24.2 // indirect
-	k8s.io/klog/v2 v2.60.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
--- keda/nodes-ready-app/main.go
+++ keda/nodes-ready-app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"log"
 	"net/http"
 	"nodes-ready-app/pkg/autoscaler"
@@ -25,6 +26,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var updateInterval = 15 * time.Second
+
 var readyNodesGauge = prometheus.NewGauge(prometheus.GaugeOpts{
 	Name: "ready_nodes",
 	Help: "Report the number of Ready nodes in the cluster.",
@@ -34,8 +37,8 @@ func init() {
 	prometheus.MustRegister(readyNodesGauge)
 }
 
-func processReadyNodes(store cache.Store) {
-	var ready float64
+func processReadyNodes(store cache.Store) (int, int, int) {
+	var ready int
 	nodes := store.List()
 	for i := range nodes {
 		if node, ok := (nodes[i].(*corev1.Node)); ok {
@@ -44,8 +47,7 @@ func processReadyNodes(store cache.Store) {
 			}
 		}
 	}
-	readyNodesGauge.Set(ready)
-	klog.Infof("%v total nodes, %v ready nodes", len(nodes), ready)
+	return len(nodes), ready, len(nodes) - ready
 }
 
 func restConfig() (*rest.Config, error) {
@@ -60,6 +62,9 @@ func restConfig() (*rest.Config, error) {
 }
 
 func main() {
+	klog.InitFlags(flag.CommandLine)
+	klog.SetOutput(os.Stderr)
+
 	clusterConfig, err := restConfig()
 	if err != nil {
 		log.Fatalf("could not get config: %v\n", err)
@@ -70,37 +75,49 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	if val := os.Getenv("UPDATE_INTERVAL"); val != "" {
+		if x, err := time.ParseDuration(val); err != nil {
+			klog.Infof("failed to parse UPDATE_INTERVAL=%q: %v; defaulting to %v\n", val, err, updateInterval)
+		} else {
+			klog.Infof("Setting update interval to %s\n", x.String())
+			updateInterval = x
+		}
+	}
+
+	klog.Infof("update interval set to %v", updateInterval.String())
+
 	mainCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	server := &http.Server{
+	httpServer := &http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
 	http.Handle("/metrics", promhttp.Handler())
 
-	factory := informers.NewSharedInformerFactory(clientSet, 0)
-	nodeInformer := factory.Core().V1().Nodes()
-	informer := nodeInformer.Informer()
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{})
-	go factory.Start(mainCtx.Done())
-	if !cache.WaitForNamedCacheSync("controller", mainCtx.Done(), informer.HasSynced) {
-		log.Fatalln("timed out waiting for caches to sync")
-	}
+	informerFactory := informers.NewSharedInformerFactory(clientSet, 0)
+	nodeInformer := informerFactory.Core().V1().Nodes()
+	sharedInformer := nodeInformer.Informer()
+	sharedInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{})
 
 	g, gCtx := errgroup.WithContext(mainCtx)
 
+	go informerFactory.Start(gCtx.Done())
+	if !cache.WaitForNamedCacheSync(os.Args[0], gCtx.Done(), sharedInformer.HasSynced) {
+		klog.Fatalln("timed out waiting for caches to sync")
+	}
+
 	g.Go(func() error {
-		return server.ListenAndServe()
+		return httpServer.ListenAndServe()
 	})
 
 	g.Go(func() error {
 		<-gCtx.Done()
-		server.SetKeepAlivesEnabled(false)
-		shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), server.WriteTimeout)
+		httpServer.SetKeepAlivesEnabled(false)
+		shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), httpServer.WriteTimeout)
 		defer shutdownRelease()
-		return server.Shutdown(shutdownCtx)
+		return httpServer.Shutdown(shutdownCtx)
 	})
 
 	g.Go(func() error {
@@ -108,8 +125,10 @@ func main() {
 			select {
 			case <-gCtx.Done():
 				return nil
-			case <-time.After(2 * time.Second):
-				processReadyNodes(informer.GetStore())
+			case <-time.After(updateInterval):
+				total, ready, unready := processReadyNodes(sharedInformer.GetStore())
+				klog.Infof("%v total nodes, %v ready nodes, %v unready nodes", total, ready, unready)
+				readyNodesGauge.Set(float64(ready))
 			}
 		}
 	})
