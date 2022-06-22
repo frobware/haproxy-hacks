eb1d70044cf5f3e8986e357119830d3248473ed7
Andrew McDermott <aim@frobware.com>
2022-06-22 15:16:09 +0100

    keda: remove spurious 'w' character

--- keda/setup/setup.sh
+++ keda/setup/setup.sh
@@ -10,7 +10,7 @@ oc get -n openshift-ingress-operator serviceaccount/thanos && oc delete -n opens
 oc create -n openshift-ingress-operator serviceaccount thanos
 oc describe -n openshift-ingress-operator serviceaccount thanos
 oc apply -n openshift-ingress-operator -f role.yaml
-secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{print $1 }'w)
+secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{ print $1 }')
 oc process TOKEN=$secret -f triggerauthentication.yaml | oc apply -n openshift-ingress-operator -f -
 oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
 oc get triggerauthentications.keda.sh -o yaml | grep $secret
