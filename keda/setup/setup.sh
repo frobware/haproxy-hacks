48fe5dee4aa780442f944e08c8d8f0010469104a
Andrew McDermott <aim@frobware.com>
2022-06-27 13:27:56 +0100

    keda: snapshot
    I can't remember what I was last doing...
    
    
    Former-commit-id: 10d93a5b8b8adfbd00d39f98453575b4ea189a66

--- keda/setup/setup.sh
+++ keda/setup/setup.sh
@@ -10,7 +10,10 @@ oc get -n openshift-ingress-operator serviceaccount/thanos && oc delete -n opens
 oc create -n openshift-ingress-operator serviceaccount thanos
 oc describe -n openshift-ingress-operator serviceaccount thanos
 oc apply -n openshift-ingress-operator -f role.yaml
-secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{ print $1 }')
+secret=$(oc get secret -n openshift-ingress-operator | grep thanos-token | head -n 1 | awk '{print $1 }')
 oc process TOKEN=$secret -f triggerauthentication.yaml | oc apply -n openshift-ingress-operator -f -
 oc adm policy add-role-to-user thanos-metrics-reader -z thanos --role-namespace=openshift-ingress-operator
 oc get triggerauthentications.keda.sh -o yaml | grep $secret
+
+#$ oc get secrets -n openshift-monitoring thanos-querier-token-gqstv -o yaml |grep token
+# secret=$(oc get secrets -n openshift-monitoring thanos-querier-token-gqstv -o yaml | token: | head -n 1 | awk '{print $1 }')
