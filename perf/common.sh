a238f7ed947d76c30960442eec644e2c021c4380
Andrew McDermott <aim@frobware.com>
2022-10-13 10:29:46 +0100

    perf/common: add domain function

--- perf/common.sh
+++ perf/common.sh
@@ -7,3 +7,8 @@ docker_pod_prefix=docker-nginx-
 function docker_pods() {
     docker ps --no-trunc --filter name=^/${docker_pod_prefix} --format '{{.Names}}'
 }
+
+function domain() {
+    local a=$(dig +short -x $(dig +search +short "$(hostname)"))
+    echo ${${a#*.*}%.}
+}
