35b37f03727035092b903dc3440a927849f2077d
Andrew McDermott <aim@frobware.com>
2022-10-19 10:24:11 +0100

    perf: snapshot

--- perf/common.sh
+++ perf/common.sh
@@ -2,6 +2,10 @@
 
 docker_pod_prefix=docker-nginx-
 
+case "$(hostname)" in
+    spicy*) docker_pod_prefix=docker_nginx_;;
+esac
+
 function docker_pods() {
     docker ps --no-trunc --filter name=^/${docker_pod_prefix} --format '{{.Names}}'
 }
