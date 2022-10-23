be372e27ad0d3e1c0583276c25ecd96a75fe9686
Andrew McDermott <aim@frobware.com>
2022-10-23 03:13:29 +0100

    backender: snap

--- perf/cmd/backender/backender.go
+++ perf/cmd/backender/backender.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"embed"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
+	"sync"
 	"syscall"
+	"time"
 
 	"github.com/frobware/haproxy-hacks/perf"
 )
 
 type Backend struct {
-	hostIPAddr      string
-	name            string
-	port            int
-	terminationType perf.TerminationType
-	listener        *net.Listener
+	HostAddr        string               `json:"host_addr"`
+	Name            string               `json:"name"`
+	Port            int                  `json:"port"`
+	TerminationType perf.TerminationType `json:"termination_type"`
 }
 
 type BackendsByTerminationType map[perf.TerminationType][]Backend
 
 // Program flags
 var (
-	hostPrefix = flag.String("host-prefxx", "http-scale", "prefix for hostname")
-	nbackends  = flag.Int("nbackends", 5, "number of backends servers to create")
+	nbackends = flag.Int("nbackends", 100, "number of backends servers to create")
 )
 
 var (
@@ -66,6 +70,20 @@ func tlsTemporaryCertFile() string {
 	return mustCreateTemporaryFile([]byte(tlsCert))
 }
 
+func toTerminationType(s string) perf.TerminationType {
+	switch s {
+	case "http":
+		return perf.HTTPTermination
+	case "edge":
+		return perf.EdgeTermination
+	case "reencrypt":
+		return perf.ReencryptTermination
+	case "passthrough":
+		return perf.PassthroughTermination
+	}
+	panic(s)
+}
+
 func mustResolveCurrentHost() string {
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -74,35 +92,12 @@ func mustResolveCurrentHost() string {
 	return hostname
 }
 
-func spawnBackend(b *Backend) {
-	htmlHandler := http.FileServer(http.FS(htmlFS))
-
-	go func() {
-		tlsCertFile := tlsTemporaryCertFile()
-		defer os.Remove(tlsCertFile)
-
-		tlsKeyFile := tlsTemporaryKeyFile()
-		defer os.Remove(tlsKeyFile)
-
-		switch b.terminationType {
-		case perf.HTTPTermination, perf.EdgeTermination:
-			if err := http.Serve(*b.listener, htmlHandler); err != nil {
-				log.Fatal(err)
-			}
-		default:
-			if err := http.ServeTLS(*b.listener, htmlHandler, tlsCertFile, tlsKeyFile); err != nil {
-				log.Fatal(err)
-			}
-		}
-	}()
-}
-
-func startMetadataServer(port int) error {
-	var backends = map[perf.TerminationType][]Backend{}
+func startMetadataServer(backendsByType BackendsByTerminationType, port int) {
+	var mu sync.Mutex
 
 	printBackendsForType := func(w io.Writer, t perf.TerminationType) error {
-		for i := 0; i < len(backends[t]); i++ {
-			if _, err := io.WriteString(w, fmt.Sprintf("%v %v %v %v\n", backends[t][i].name, backends[t][i].hostIPAddr, backends[t][i].port, t)); err != nil {
+		for _, b := range backendsByType[t] {
+			if _, err := io.WriteString(w, fmt.Sprintf("%v %v %v %v\n", b.Name, b.HostAddr, b.Port, b.TerminationType)); err != nil {
 				return err
 			}
 		}
@@ -112,56 +107,100 @@ func startMetadataServer(port int) error {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/backends", func(w http.ResponseWriter, r *http.Request) {
-		for _, t := range perf.AllTerminationTypes[:] {
-			printBackendsForType(w, t)
+		mu.Lock()
+		defer mu.Unlock()
+		switch r.Method {
+		case "POST":
+			decoder := json.NewDecoder(r.Body)
+			decoder.DisallowUnknownFields()
+			var x Backend
+			if err := decoder.Decode(&x); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			words := strings.Split(x.Name, "-")
+			if len(words) != 4 {
+				http.Error(w, "expected 4 words", http.StatusBadRequest)
+				return
+			}
+			i, err := strconv.ParseInt(words[3], 10, 64)
+			if err != nil {
+				http.Error(w, "expected 4 words", http.StatusBadRequest)
+				return
+			}
+			backendsByType[x.TerminationType][i].Port = x.Port
+		default:
+			for _, t := range perf.AllTerminationTypes[:] {
+				printBackendsForType(w, t)
+			}
 		}
 	})
+
 	mux.HandleFunc("/backends/edge", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
 		printBackendsForType(w, perf.EdgeTermination)
 	})
+
 	mux.HandleFunc("/backends/http", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
 		printBackendsForType(w, perf.HTTPTermination)
 	})
+
 	mux.HandleFunc("/backends/passthrough", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
 		printBackendsForType(w, perf.PassthroughTermination)
 	})
+
 	mux.HandleFunc("/backends/reencrypt", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
 		printBackendsForType(w, perf.ReencryptTermination)
 	})
 
 	if err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%v", port), mux); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func startBackends(backendTypes BackendsByTerminationType, pipeRdr, pipeWr *os.File) {
-	var children = []int{}
-
-	if err := startMetadataServer(2000); err != nil {
 		log.Fatal(err)
 	}
+}
 
+func startBackends(backendsByType BackendsByTerminationType, port int) {
 	log.SetPrefix(fmt.Sprintf("[P %v] ", os.Getpid()))
 	log.Printf("pid: %d, ppid: %d, args: %s", os.Getpid(), os.Getppid(), os.Args)
 
 	go func() {
 		sigc := make(chan os.Signal, 1)
+		// if any child exits then consider that fatal for the
+		// parent too.
 		signal.Notify(sigc, syscall.SIGCHLD)
 		log.Println(<-sigc)
 		os.Exit(1)
 	}()
 
-	var i = 0
-	for t := range backendTypes {
-		i += 1
-		for _, backend := range backendTypes[t] {
-			args := append(os.Args, fmt.Sprintf("#child_%d", i))
-			childEnv := []string{fmt.Sprintf("BACKEND=%s", backend.name)}
+	go startMetadataServer(backendsByType, port)
+
+	var children []int
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer r.Close()
+	defer w.Close()
+
+	for t, backends := range backendsByType {
+		for _, backend := range backends {
+			childEnv := []string{
+				fmt.Sprintf("CHILD_ID=%v", backend.Name),
+				fmt.Sprintf("CHILD_BACKEND_NAME=%v", backend.Name),
+				fmt.Sprintf("CHILD_BACKEND_TERMINATION_TYPE=%v", t),
+			}
+			args := append(os.Args, fmt.Sprintf("#%v", backend.Name))
 			child, err := syscall.ForkExec(args[0], args, &syscall.ProcAttr{
 				Env:   append(os.Environ(), childEnv...),
-				Files: []uintptr{0, 1, 2, pipeRdr.Fd()},
+				Files: []uintptr{0, 1, 2, r.Fd()},
 			})
 			if err != nil {
 				log.Fatal(err)
@@ -172,55 +211,91 @@ func startBackends(backendTypes BackendsByTerminationType, pipeRdr, pipeWr *os.F
 		}
 	}
 
-	// for i := 0; i < fork; i++ {
-	// 	args := append(os.Args, fmt.Sprintf("#child_%d_of_%d", i, fork))
-	// 	childEnv := []string{
-	// 		fmt.Sprintf("CHILD_ID=%d", i),
-	// 	}
-	// 	pwd, err := os.Getwd()
-	// 	if err != nil {
-	// 		log.Fatalf("getwd err: %s", err)
-	// 	}
-	// 	child, err := syscall.ForkExec(args[0], args, &syscall.ProcAttr{
-	// 		Dir:   pwd,
-	// 		Env:   append(os.Environ(), childEnv...),
-	// 		Files: []uintptr{0, 1, 2, pipeWr.Fd(), parentSentinel.Fd()},
-	// 	})
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	if child != 0 {
-	// 		children = append(children, child)
-	// 	}
-	// }
-
-	// log.Printf("children %+v", children)
-	// buf := bufio.NewReader(pipeRd)
-
-	// var remaining = len(children)
-
-	// for remaining > 0 {
-	// 	line, _, err := buf.ReadLine()
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	log.Println(remaining, string(line), err)
-	// 	remaining -= 1
-	// }
-
-	log.Println("Waiting for READY!")
-
 	select {}
 }
 
-func serveBackend(backend Backend, parentFD uintptr) {
-	log.SetPrefix(fmt.Sprintf("[c %v] ", os.Getpid()))
-	// n, err := io.WriteString(os.NewFile(parentFD, "<pipe>"), fmt.Sprintf("%v\n", os.Getpid()))
-	// if err != nil {
-	// 	log.Fatalf("fatal write: n=%v %v\n", n, err)
+func serveBackend(name, terminationType string) {
+	log.SetPrefix(fmt.Sprintf("[c %v %s] ", os.Getpid(), name))
+
+	var t perf.TerminationType = toTerminationType(terminationType)
+
+	l, err := net.Listen("tcp", "0.0.0.0:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// log.Printf("%s %v\n", name, l.Addr().(*net.TCPAddr).Port)
+
+	tlsCertFile := tlsTemporaryCertFile()
+	defer os.Remove(tlsCertFile)
+
+	tlsKeyFile := tlsTemporaryKeyFile()
+	defer os.Remove(tlsKeyFile)
+
+	go func() {
+		switch t {
+		case perf.HTTPTermination, perf.EdgeTermination:
+			if err := http.Serve(l, http.FileServer(http.FS(htmlFS))); err != nil {
+				log.Fatal(err)
+			}
+		default:
+			if err := http.ServeTLS(l, http.FileServer(http.FS(htmlFS)), tlsCertFile, tlsKeyFile); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	backend := Backend{
+		HostAddr:        "127.0.0.1",
+		Name:            name,
+		Port:            l.Addr().(*net.TCPAddr).Port,
+		TerminationType: t,
+	}
+
+	jsonValue, err := json.Marshal(backend)
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		resp    *http.Response
+		retries int = 17
+	)
+
+	for retries > 0 {
+		resp, err = http.Post("http://127.0.0.1:2000/backends", "application/json", bytes.NewBuffer(jsonValue))
+		if err != nil {
+			retries -= 1
+		} else {
+			break
+		}
+		time.Sleep(42 * time.Millisecond)
+	}
+
+	if resp != nil {
+		defer resp.Body.Close()
+		_, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Printf("status = %v, data = %s\n", resp.Status, data)
+	}
+
+	// reader := bufio.NewReader(os.NewFile(3, "<pipe>"))
+
+	// for {
+	// 	n, err := io.Copy(os.Stdout, reader)
+	// 	if err != nil && err == io.EOF {
+	// 		log.Println(err)
+	// 	}
+	// 	log.Fatal(n, err)
 	// }
-	n, err := os.NewFile(4, "<pipe>").Read(make([]byte, 1))
-	log.Println("parent pipe wokeup", n, err)
+
+	// reader := bufio.NewReader(os.NewFile(3, "<pipe>"))
+	// n, err := io.Copy(os.Stdout, reader)
+	// log.Println(n, err)
+	n, err := os.NewFile(3, "<pipe>").Read(make([]byte, 1))
+	log.Println(n, err)
 	os.Exit(2)
 }
 
@@ -229,163 +304,24 @@ func main() {
 	flag.Parse()
 
 	hostIPAddr := mustResolveCurrentHost()
-	backends := map[perf.TerminationType][]Backend{}
+	allBackends := BackendsByTerminationType{}
 
 	for _, t := range perf.AllTerminationTypes {
 		for i := 0; i < *nbackends; i++ {
-			ln, err := net.Listen("tcp", "0.0.0.0:0")
-			if err != nil {
-				log.Fatal(err)
+			backend := Backend{
+				HostAddr:        hostIPAddr,
+				Name:            fmt.Sprintf("%s-%v-%v", "perf-scale", t, i),
+				TerminationType: t,
 			}
-			backends[t] = append(backends[t], Backend{
-				hostIPAddr:      hostIPAddr,
-				name:            fmt.Sprintf("%s-%v-%v", *hostPrefix, t, i),
-				port:            ln.Addr().(*net.TCPAddr).Port,
-				terminationType: t,
-				listener:        &ln,
-			})
+			allBackends[t] = append(allBackends[t], backend)
 		}
 	}
 
 	if _, isChild := os.LookupEnv("CHILD_ID"); !isChild {
-		pipeRd, pipeWr, err := os.Pipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		startBackends(backends, pipeRd, pipeWr)
-		// for i := 0; i < fork; i++ {
-		// 	args := append(os.Args, fmt.Sprintf("#child_%d_of_%d", i, fork))
-		// 	childEnv := []string{
-		// 		fmt.Sprintf("CHILD_ID=%d", i),
-		// 	}
-		// 	pwd, err := os.Getwd()
-		// 	if err != nil {
-		// 		log.Fatalf("getwd err: %s", err)
-		// 	}
-		// 	child, err := syscall.ForkExec(args[0], args, &syscall.ProcAttr{
-		// 		Dir:   pwd,
-		// 		Env:   append(os.Environ(), childEnv...),
-		// 		Files: []uintptr{0, 1, 2, pipeWr.Fd(), parentSentinel.Fd()},
-		// 	})
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-		// 	if child != 0 {
-		// 		children = append(children, child)
-		// 	}
-		// }
-
-		// log.Printf("children %+v", children)
-		// buf := bufio.NewReader(pipeRd)
-
-		// var remaining = len(children)
-
-		// for remaining > 0 {
-		// 	line, _, err := buf.ReadLine()
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-		// 	log.Println(remaining, string(line), err)
-		// 	remaining -= 1
-		// }
-
-		// log.Println(remaining, "; all children are READY!")
+		startBackends(allBackends, 2000)
 	} else {
-		// serveBackend(3)
-
-		// log.SetPrefix(fmt.Sprintf("[c %v] ", os.Getpid()))
-		// n, err := io.WriteString(os.NewFile(3, "<pipe>"), fmt.Sprintf("%v\n", os.Getpid()))
-		// if err != nil {
-		// 	log.Fatalf("fatal write: n=%v %v\n", n, err)
-		// }
-		// n, err = os.NewFile(4, "<pipe>").Read(make([]byte, 1))
-		// log.Println("parent pipe wokeup", n, err)
-		// os.Exit(2)
+		backend, _ := os.LookupEnv("CHILD_BACKEND_NAME")
+		terminationType, _ := os.LookupEnv("CHILD_BACKEND_TERMINATION_TYPE")
+		serveBackend(backend, terminationType)
 	}
-}
-
-func mainx() {
-	flag.Parse()
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-
-	backends := map[perf.TerminationType][]Backend{}
-
-	for _, t := range perf.AllTerminationTypes {
-		for i := 0; i < *nbackends; i++ {
-			ln, err := net.Listen("tcp", "0.0.0.0:0")
-			if err != nil {
-				log.Fatal(err)
-			}
-			backends[t] = append(backends[t], Backend{
-				//hostIPAddr:      hostIPAddr,
-				name:            fmt.Sprintf("%s-%v-%v", *hostPrefix, t, i),
-				port:            ln.Addr().(*net.TCPAddr).Port,
-				terminationType: t,
-				listener:        &ln,
-			})
-		}
-	}
-
-	//	hostIPAddr := mustResolveCurrentHost()
-
-	// tlsCertFile := tlsTemporaryCertFile()
-	// defer os.Remove(tlsCertFile)
-
-	// tlsKeyFile := tlsTemporaryKeyFile()
-	// defer os.Remove(tlsKeyFile)
-
-	// for _, t := range perf.AllTerminationTypes {
-	// 	for i := 0; i < *nbackends; i++ {
-	// 		ln, err := net.Listen("tcp", "0.0.0.0:0")
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		backends[t] = append(backends[t], Backend{
-	// 			hostIPAddr:      hostIPAddr,
-	// 			name:            fmt.Sprintf("%s-%v-%v", *hostPrefix, t, i),
-	// 			port:            ln.Addr().(*net.TCPAddr).Port,
-	// 			terminationType: t,
-	// 			listener:        &ln,
-	// 		})
-	// 		// go func(t perf.TerminationType, l *net.Listener) {
-	// 		// 	switch t {
-	// 		// 	case perf.HTTPTermination, perf.EdgeTermination:
-	// 		// 		if err := http.Serve(*l, htmlHandler); err != nil {
-	// 		// 			log.Fatal(err)
-	// 		// 		}
-	// 		// 	default:
-	// 		// 		if err := http.ServeTLS(*l, htmlHandler, tlsCertFile, tlsKeyFile); err != nil {
-	// 		// 			log.Fatal(err)
-	// 		// 		}
-	// 		// 	}
-	// 		// }(t, &ln)
-	// 	}
-	// 	printBackendConnectionInfo(os.Stdout, t)
-	// }
-
-	// mux := http.NewServeMux()
-
-	// mux.HandleFunc("/backends", func(w http.ResponseWriter, r *http.Request) {
-	// 	for _, t := range perf.AllTerminationTypes[:] {
-	// 		printBackendConnectionInfo(w, t)
-	// 	}
-	// })
-	// mux.HandleFunc("/backends/edge", func(w http.ResponseWriter, r *http.Request) {
-	// 	printBackendConnectionInfo(w, perf.EdgeTermination)
-	// })
-	// mux.HandleFunc("/backends/http", func(w http.ResponseWriter, r *http.Request) {
-	// 	printBackendConnectionInfo(w, perf.HTTPTermination)
-	// })
-
-	// mux.HandleFunc("/backends/passthrough", func(w http.ResponseWriter, r *http.Request) {
-	// 	printBackendConnectionInfo(w, perf.PassthroughTermination)
-	// })
-	// mux.HandleFunc("/backends/reencrypt", func(w http.ResponseWriter, r *http.Request) {
-	// 	printBackendConnectionInfo(w, perf.ReencryptTermination)
-	// })
-
-	// if err := http.ListenAndServe(":2000", mux); err != nil {
-	// 	log.Fatal(err)
-	// }
 }
