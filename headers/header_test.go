62004e069d9122be8fe70546aa3fe514239ca5cc
Andrew McDermott <aim@frobware.com>
2023-02-23 13:06:48 +0000

    headers: additional tests

--- headers/header_test.go
+++ headers/header_test.go
@@ -39,13 +39,14 @@ func TestSamples(t *testing.T) {
 		{description: "empty quoted value", input: `%{+Q}`, valid: false},
 		{description: "quoted value", input: `%{+Q}foo`, valid: false},
 		{description: "request hdr with empty field", input: `%[req.hdr()]`, valid: false},
-		{description: "request hdr with percent field", input: `%[req.hdr(%)]`, valid: false},
+		{description: "request hdr with percent field", input: `%[req.hdr(%)]`, valid: true},
 		{description: "request hdr with known field", input: `%[req.hdr(Host)]`, valid: true},
 		{description: "request hdr with syntax error", input: `%[req.hdr(Host]`, valid: false},
 		{description: "incomplete expression", input: `%[req`, valid: false},
 		{description: "quoted field", input: `%[req.hdr(%{+Q}Host)]`, valid: true},
 		{description: "value with conditional expression", input: `%[req.hdr(Host)] if foo`, valid: false},
 		{description: "value with what looks like a conditional expression", input: `%[req.hdr(Host)]\ if\ foo`, valid: true},
+		{description: "unsuported fetcher", input: `%[date(3600),http_date`, valid: false},
 	}
 
 	for _, tc := range tests {
