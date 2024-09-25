f95a9bdcc4bc72e0a70e27b992677bf73a9aed11
Andrew McDermott <aim@frobware.com>
2024-09-25 15:26:38 +0100

    OCPBUGS-40850/check-routes: add single-te, duplicate-te, healthz

--- OCPBUGS-40850/check-routes.bash
+++ OCPBUGS-40850/check-routes.bash
@@ -13,18 +13,20 @@ oc get routes --no-headers -o custom-columns=HOST:.spec.host | while read -r hos
         # curl version 8.5.0 and earlier allowed duplicate
         # Transfer-Encoding headers. Later versions started rejecting
         # these headers.
-        output=$(${CURL:-curl-8.5.0} --no-keepalive -L -I -sS -o /dev/null -w "%{http_code}" -k "https://${host}/healthz")
-        if [ $? -ne 0 ]; then
-            exit $?
-        fi
+        for path in healthz single-te duplicate-te; do
+            output=$(${CURL:-curl-8.5.0} --no-keepalive -L -I -sS -o /dev/null -w "%{http_code}" -k "https://${host}/$path")
+            if [ $? -ne 0 ]; then
+                exit $?
+            fi
 
-        read -r http_code <<< "$output"
+            read -r http_code <<< "$output"
 
-        if [ $http_code -eq 200 ]; then
-            echo "$http_code $host"
-        else
-            echo "$http_code: **** $host check FAILED ****" >&2
-        fi
+            if [ $http_code -eq 200 ]; then
+                echo "$http_code $host/$path"
+            else
+                echo "$http_code: **** $host/$path check FAILED ****" >&2
+            fi
+        done
     else
         echo "No route found."
     fi
